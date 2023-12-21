df0bdb9ec8b693c5bb8924e9f10660645f32ecd3
prasanna <32860910+prasannavasudevan@users.noreply.github.com>
2023-12-21 11:34:36 +0530

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'VSC clone using Tailwind',
+    description: "Successfully recreated the Visual Studio landing page through meticulous implementation of HTML and CSS, utilizing the Tailwind CSS framework.Demonstrated proficiency in frontend development by accurately replicating the design elements, layout, and responsiveness of the original page.",
+      image: '/images/vsclone.png',
+      tags: ['Html', 'CSS', 'Tailwind'],
+    source: 'https://github.com/prasannavasudevan/fsjs2.0/tree/main/Projects(Tailwind)',
+    output: 'https://github.com/prasannavasudevan/fsjs2.0/blob/main/Projects(Tailwind)/output.png',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce Merchant using MERN',
+    description:"Designed and developed a full-stack E-commerce platform leveraging the MERN (MongoDB, Express.js, React, Node.js) stack. The platform incorporates advanced authentication and authorization, which will ensure the security of users' data and restrict access to specific parts of the application to authorized users only. ",
+    image: '/images/mernProject.png',
+    tags: ['MongoDB', 'Express', 'React', 'NodeJS'],
+    source: 'https://github.com/prasannavasudevan/MERN-project/tree/main/E-commerce%20merchant%20site%20',
+    output: 'https://github.com/prasannavasudevan/MERN-project/blob/main/README.md',
+    id: 1,
+  },
+  {
+    title: 'RGB Color Game using Javascript',
+    description: "Designed and implemented an interactive RGB Color Game using HTML, CSS, and JavaScript. Developed a dynamic application that generates random RGB colors for users to match by selecting the precise pixel values.",
+      image: '/images/3.jpg',
+      tags: ['Html', 'CSS', 'Javascript'],
+    source: 'https://google.com',
+    output: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'GitApp using firebase',
+    description: "Designed and implemented a GitApp utilizing Firebase for real-time data storage and retrieval. Users input GitHub usernames, triggering an API call to fetch and display pertinent details such as repositories, followers, and contributions.",
+    image: '/images/gitApp.png',
+    tags: ['React', 'MongoDB', 'Firebase'],
+    source: 'https://github.com/prasannavasudevan/React-projects/tree/main/Github%20app%20with%20firebase',
+    output: 'https://github.com/prasannavasudevan/React-projects/blob/main/Github%20app%20with%20firebase/readme.md',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Started my journey', },
+  { year: 2020, text: 'Worked on multiple front-end projects', },
+  { year: 2021, text: 'Founded JavaScript Mastery', },
+  { year: 2022, text: 'Shared my projects with the world', },
+  { year: 2023, text: 'Started my own portfolio', },
+];
