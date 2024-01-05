1ff742343d99ec2fa947826570e537f730870eab
Muhesh7 <vmspride7@gmail.com>
2024-01-05 23:46:30 +0530

    workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/portfolio' : '', // Set your repository name
-  assetPrefix: isProd ? '/portfolio/' : '', // Set your repository name
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add your custom webpack configurations here
-
-    return config;
-  },
-};
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+    assetPrefix: './',
+  };
+  
+  export default nextConfig;
