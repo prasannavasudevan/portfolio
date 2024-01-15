11e4b431ef50b7e9ab14c9c2d34f8aa5a1db14d0
indreshp135 <indreshp135@gmail.com>
2024-01-16 00:52:42 +0530

    image basepath

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'VSC clone using Tailwind',
     description: "Successfully recreated the Visual Studio landing page through meticulous implementation of HTML and CSS, utilizing the Tailwind CSS framework.Demonstrated proficiency in frontend development by accurately replicating the design elements, layout, and responsiveness of the original page.",
-      image: '/images/vsclone.png',
+      image: './images/vsclone.png',
       tags: ['Html', 'CSS', 'Tailwind'],
     source: 'https://github.com/prasannavasudevan/fsjs2.0/tree/main/Projects(Tailwind)',
     output: 'https://github.com/prasannavasudevan/fsjs2.0/blob/main/Projects(Tailwind)/output.png',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'E-Commerce Merchant using MERN',
     description:"Designed and developed a full-stack E-commerce platform leveraging the MERN (MongoDB, Express.js, React, Node.js) stack. The platform incorporates advanced authentication and authorization, which will ensure the security of users' data and restrict access to specific parts of the application to authorized users only. ",
-    image: '/images/mernProject.png',
+    image: './images/mernProject.png',
     tags: ['MongoDB', 'Express', 'React', 'NodeJS'],
     source: 'https://github.com/prasannavasudevan/MERN-project/tree/main/E-commerce%20merchant%20site%20',
     output: 'https://github.com/prasannavasudevan/MERN-project/blob/main/README.md',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Virtual piano using DOM',
     description: "Designed and implemented a Virtual Piano using DOM (Document Object Model) manipulation, enabling users to play musical notes through an interactive web interface. The project incorporated dynamic features by integrating random color generation in real-time on the piano keys while playing.",
-      image: '/images/piano.png',
+      image: './images/piano.png',
       tags: ['Html', 'CSS', 'Javascript'],
     source: 'https://github.com/prasannavasudevan/Projects-DOM-/tree/main/Virtual%20Piano(DOM)',
     output: 'https://github.com/prasannavasudevan/Projects-DOM-/blob/main/Virtual%20Piano(DOM)/readme.md',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'GitApp using firebase',
     description: "Designed and implemented a GitApp utilizing Firebase for real-time data storage and retrieval. Users input GitHub usernames, triggering an API call to fetch and display pertinent details such as repositories, followers, and contributions.",
-    image: '/images/gitApp.png',
+    image: './images/gitApp.png',
     tags: ['React', 'MongoDB', 'Firebase'],
     source: 'https://github.com/prasannavasudevan/React-projects/tree/main/Github%20app%20with%20firebase',
     output: 'https://github.com/prasannavasudevan/React-projects/blob/main/Github%20app%20with%20firebase/readme.md',
